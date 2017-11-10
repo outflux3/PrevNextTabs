dbc7f34fc22afe39b677625c79669f1224771688
Marc <mw@ajcg.org>
2017-11-09 22:37:57 -0500

    AdminThemeUIKit support

--- PrevNextTabs.js
+++ PrevNextTabs.js
@@ -2,3 +2,14 @@
     $("#_ProcessPageEditNext").tooltip();
     $("#_ProcessPageEditPrev").tooltip();
  });
+
+
+$(document).ready(function() {
+	$('#_ProcessPageEditNext').click(function(e) {
+		e.stopPropagation();
+	});
+
+	$('#_ProcessPageEditPrev').click(function(e) {
+		e.stopPropagation();
+	});
+});
