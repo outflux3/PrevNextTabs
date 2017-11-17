73b8b881796420a4d591d86ab044f3e0b787f967
Marc <mw@ajcg.org>
2017-11-16 20:18:38 -0500

    Prev/Next Links in Breadcrumbs Option
    also:
    - add sort=sort, so that the prev/next links go in the correct order
    - access key for breadcrumb links (e.g. ctrl+option+> for next)
    - wraparound navigation (last item can link to first, as next link)

--- PrevNextTabs.js
+++ PrevNextTabs.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
 
+	// UiKit prev/next in breadcrumbs area
+	$('.prev-next-links .uk-pagination-previous a').tooltip();
+	$('.prev-next-links .uk-pagination-next a').tooltip();
+
+	// Reno prev/next in breadcrumbs area
+	$('.prev-next-links .pagination-previous a').tooltip();
+	$('.prev-next-links .pagination-next a').tooltip();
+
+	// Prev/Next in Tabs -----
  	var nextLink = $("#_ProcessPageEditNext"),
  		prevLink = $("#_ProcessPageEditPrev");
 
  	nextLink.tooltip();
 	prevLink.tooltip();
 
+	// UiKit Required Mods -----
 	nextLink.parent().addClass('nextLink');
 	prevLink.parent().addClass('prevLink');
 
