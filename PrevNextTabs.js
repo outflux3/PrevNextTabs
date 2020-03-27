5f021d13c8c289fde0fb9716a1c78f2e9537604a
outflux3 <marc@nibiri.com>
2020-03-27 11:53:07 -0400

    Rewrite of module to use ajax.
    See the README for more info.

--- PrevNextTabs.js
+++ PrevNextTabs.js
@@ -1,21 +1,75 @@
+
+
 $(document).ready(function() {
 
-	// UiKit prev/next in breadcrumbs area
-	$('.prev-next-links .uk-pagination-previous a').tooltip();
-	$('.prev-next-links .uk-pagination-next a').tooltip();
+	$('uk-pagination').addClass('InputfieldIgnoreChanges');
+	
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	var id = urlParams.get('id');
+	var url = '?prevNextTabs='+id;
+	
+	$.get(url, function(response) {
+		console.log(response);
+		var next = response.next;
+		var prev = response.prev;
+		var first = response.first;
+		var last = response.last;
 
-	// Reno prev/next in breadcrumbs area
-	$('.prev-next-links .pagination-previous a').tooltip();
-	$('.prev-next-links .pagination-next a').tooltip();
+		// Section 1: Breadcrumbs, UiKit
+		$('.prev-next-links .uk-pagination-previous a i').removeClass('fa-spinner fa-spin').addClass('fa-angle-left');
+		$('.prev-next-links .uk-pagination-next a i').removeClass('fa-spinner fa-spin').addClass('fa-angle-right');
+
+		// Section 2: Breadcrumbs, Reno
+		$('.prev-next-links .pagination-previous a i').removeClass('fa-spinner fa-spin').addClass('fa-angle-left');
+		$('.prev-next-links .pagination-next a i').removeClass('fa-spinner fa-spin').addClass('fa-angle-right');
+
+		// Section 3: Tabs
+		$('#_ProcessPageEditPrev i').removeClass('fa-spinner fa-spin').addClass('fa-angle-double-left');
+		$('#_ProcessPageEditNext i').removeClass('fa-spinner fa-spin').addClass('fa-angle-double-right');
+
+		if(prev) {	
+			$('.prev-next-links .uk-pagination-previous a, .prev-next-links .pagination-previous a, #_ProcessPageEditPrev')
+			.prop('href', config.urls.admin + 'page/edit/?id='+prev)
+			.prop('title', response.prevTitle);	
+		} else {
+			$('.prev-next-links .uk-pagination-previous a, .prev-next-links .pagination-previous a, #_ProcessPageEditPrev')
+			.prop('href', config.urls.admin + 'page/edit/?id='+last)
+			.prop('title', response.lastTitle);
+		}
+		
+		if(next) {
+			$('.prev-next-links .uk-pagination-next a, .prev-next-links .pagination-next a, #_ProcessPageEditNext')
+			.prop('href', config.urls.admin + 'page/edit/?id='+next)
+			.prop('title', response.nextTitle);
+		} else {
+			$('.prev-next-links .uk-pagination-next a, .prev-next-links .pagination-next a, #_ProcessPageEditNext')
+			.prop('href', config.urls.admin + 'page/edit/?id='+first)
+			.prop('title', response.firstTitle);
+		}
+
+		$('.prev-next-links .uk-pagination-previous a').tooltip();
+		$('.prev-next-links .uk-pagination-next a').tooltip();
+	
+		// Reno prev/next in breadcrumbs area
+		$('.prev-next-links .pagination-previous a').tooltip();
+		$('.prev-next-links .pagination-next a').tooltip();
+
+		// Prev/Next in Tabs -----
+		var nextLink = $("#_ProcessPageEditNext"),
+			prevLink = $("#_ProcessPageEditPrev");
+
+		nextLink.tooltip();
+		prevLink.tooltip();
+
+	},"json");
+
 
 	// Prev/Next in Tabs -----
- 	var nextLink = $("#_ProcessPageEditNext"),
- 		prevLink = $("#_ProcessPageEditPrev");
+	var nextLink = $("#_ProcessPageEditNext"),
+		prevLink = $("#_ProcessPageEditPrev");
 
- 	nextLink.tooltip();
-	prevLink.tooltip();
-
-	// UiKit Required Mods -----
+	// UiKit Required Mods for Tabs -----
 	nextLink.parent().addClass('nextLink');
 	prevLink.parent().addClass('prevLink');
 
--- README.md
+++ README.md
@@ -3,11 +3,17 @@ PrevNextTabs module for ProcessWire
 
 Processwire helper modules for adding page navigation within the editor. [ProcessWire CMS](http://processwire.com/).
 
-Currently beta state - please use with caution and report all errors or things that don't work as expected.
 
 ## Overview
 
-This is a very simple module that adds **Previous** and **Next** links inline with the tabs on the page editor.
+This is a simple module that adds **Previous** and **Next** links to the page editor, for the purpose of allowing editors to navigate from one page to the next.
+
+The links may be placed in the breadcrumbs, or in the editor tabs (on the right side).
+
+The latest version avoids potential delays and overhead to the page editor load time, by offloading the work of getting the previous and next page ids to an ajax request, which upon completion populates the links with the URL and title of the adjacent pages.
+
+As such this module should not cause any additional overhead to the page editor, aside from loading the CSS and JS. The links themselves become usable after the ajax request is complete, and this duration can take anywhere from .5 seconds up to 2 or more seconds depending on how many sibling pages exist. The module has been tested on sites with up to 9,000 sibling pages.
+
 Hovering over the tab shows the title of the previous or next page (using the admin's built in jqueryUI tooltips.)
 
 ### Usage
@@ -22,4 +28,6 @@ If you are using PW version 2.6.1 or later, the system will prevent you from lea
 For earlier versions, to avoid accidentally losing changes made to a page that might occur if a user accidentally clicks on one of these, make sure to have the Form Save Reminder module installed.
 
 
-![Desc](https://raw.githubusercontent.com/outflux3/PrevNextTabs/master/screen.png)
+### Changelog
+
+- .016: Complete module rewrite - The module no longer runs the $pages->find() within the hook, which could cause load delays. Module only outputs template markup which is modified client side by the results of the ajax request to the module, to obtain the links.
